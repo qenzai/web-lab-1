bef07fe0f8747239dcb19f312e2abf69c342cfec
tykan <143395188+qenzai@users.noreply.github.com>
2025-03-12 16:14:41 +0200

    Delete script1.js

--- script1.js
+++
@@ -1,5 +0,0 @@
-function reverseNumber() {
-    let num = document.getElementById("numberInput").value;
-    let reversed = num.split('').reverse().join('');
-    document.getElementById("result").innerText = "Результат: " + reversed;
-}
