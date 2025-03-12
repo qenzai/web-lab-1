d816ad1e5f5eb5ddefd1644e6d52f86bc49799dd
tykan <143395188+qenzai@users.noreply.github.com>
2025-03-12 16:15:33 +0200

    Delete script3.js

--- script3.js
+++
@@ -1,6 +0,0 @@
-function findLongestWord() {
-    let text = document.getElementById("textInput").value;
-    let words = text.split(/\s+/);
-    let longest = words.reduce((a, b) => (a.length > b.length ? a : b), "");
-    document.getElementById("output").innerText = `Найдовше слово: "${longest}" (${longest.length} літер)`;
-}
